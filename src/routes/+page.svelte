b1e1be384f1a68725fd3c0e2bcb95fc44278f44f
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2024-09-13 15:03:59 +0200

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,11 +14,42 @@
         <details name='weeks'>
             <summary>Week 1</summary>
             <ul>
-                <li>Maandag</li>
-                <li>Dinsdag</li>
-                <li>Woensdag</li>
-                <li>Donderdag</li>
-                <li>Vrijdag</li>
+                <li>
+                    <h3>Maandag</h3>
+                    <dl>
+                        <dt>We hebben de introductie gehad en zijn verdeeld in groepjes voor onze squad page.</dt>
+                        <dt>Vervolgens afgesproken woensdag allemaal een lofi-design af te hebben.</dt>
+                    </dl>
+                </li>                
+                <li>
+                    <h3>Dinsdag</h3>
+                    <dl>
+                        <dt>Lofi-design gemaakt in Figma.</dt>
+                        <dl><a href="https://github.com/rutgerkock/your-tribe-for-life-squad-page/issues/1">Zie issue</a></dl>
+                        <dt>Svelte/Directus documentatie doorgelezen.</dt>
+                    </dl>
+                </li>                   
+                <li>
+                    <h3>Woensdag</h3>
+                    <dl>
+                        <dt>We hebben de introductie gehad en zijn verdeeld in groepjes voor onze squad page.</dt>
+                        <dt>Vervolgens afgesproken woensdag allemaal een lofi-design af te hebben.</dt>
+                    </dl>
+                </li>                   <li>
+                    <h3>Donderdag</h3>
+                    <dl>
+                        <dt>Lofi-design gemaakt in Figma.</dt>
+                        <dl><a href="https://github.com/rutgerkock/your-tribe-for-life-squad-page/issues/1">Zie issue</a></dl>
+                        <dt>Svelte/Directus documentatie doorgelezen.</dt>
+                    </dl>
+                </li>                  
+                <li>
+                    <h3>Vrijdag</h3>
+                    <dl>
+                        <dt>We hebben de introductie gehad en zijn verdeeld in groepjes voor onze squad page.</dt>
+                        <dt>Vervolgens afgesproken woensdag allemaal een lofi-design af te hebben.</dt>
+                    </dl>
+                </li>               
             </ul>
         </details>
         <details name='weeks'>
@@ -83,7 +114,7 @@
         background-color: var(--background-color);
         width: 100%;
         padding: 1rem 4rem;
-        z-index: 997;
+        z-index: 996;
     }
 
     body {
@@ -97,7 +128,7 @@
         min-height: 100vh;
         display: flex;
         flex-direction: column;
-        padding: 5rem 4rem;
+        padding: 7rem 4rem;
         scroll-snap-align: start; 
     }
 
@@ -110,15 +141,15 @@
     }
 
     section h1 {
-    font-size: 2rem;
-    margin: 2rem 0rem;
-    color: var(--blue-color);
-    position: sticky;
-    z-index: 998; 
-    width: 100%;
-    background-color: var(--background-color);
-    top: 0.5rem;
-}
+        font-size: 2rem;
+        margin: 2rem 0rem;
+        color: var(--blue-color);
+        position: sticky;
+        z-index: 997; 
+        width: 100%;
+        background-color: var(--background-color);
+        top: 0.5rem;
+    }
 
     section h2 {
         margin-top: 1.5rem;
@@ -133,9 +164,9 @@
     }
 
     section ul {
-        position: sticky;
         width: 70%;
-        margin-left: 16rem;
+        margin: 2rem 1rem;
+        width: 70%;
     }
 
     section ul li {
@@ -143,17 +174,5 @@
         padding: 1rem 0rem;
         color: var(--yellow-color);
     }
-
-    #fdnd {
-        display: fixed;
-        right: 0;
-        top: 0;
-        position: fixed;
-        background-color: var(--background-color);
-        padding: 1rem 4rem;
-        z-index: 999;
-    }
-
-
 
 </style>
