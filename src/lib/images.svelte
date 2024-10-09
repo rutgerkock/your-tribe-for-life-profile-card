9a3231e5098ab1bdcb8bbcd8d9ea31779529281b
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2024-10-09 13:15:33 +0200

    test image 12394

--- src/lib/images.svelte
+++ src/lib/images.svelte
@@ -35,13 +35,14 @@
         <button popovertarget={`popover-${folder.name}`} title="Close">
             <div id={`popover-${folder.name}`} class="popover" popover>
                 {#each generateImagePaths(folder) as image}
-                <img src={`/images/${image}`} alt={folder.name} loading="lazy" width="300" height="200" />
+                    <img loading="lazy" src={`/images/${image}`} alt={folder.name} />
+                    
                 {/each}
             </div>
         </button>
     </li>
 {/each}
-    
+
 
 <style>
     li {
